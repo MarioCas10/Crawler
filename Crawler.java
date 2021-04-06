da9f15ae973bd67fcc0ffed849dc7bdffd3d7e92
= <=>
2021-04-06 13:05:08 +0200

    Cambio Crawler3

--- Crawler.java
+++ Crawler.java
@@ -1,12 +1,38 @@
 import Crawler.*;
+
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 
 public class Crawler {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
         
         Acceso_Fichero entrada = new Acceso_Fichero();
         entrada.read();
-        
-    }
 
+        String Crawler = System.getProperty("user.dir")+"/";
+	    //long inicio = System.currentTimeMillis();
+	    String Url = "https://example.com";
+	    String Text = "urls.txt";
+	    int codigo = StatusCode.getStatusConnectionCode(Url);
+	    if(codigo==200){
+	        crearOComprobarArchivo(Url, Crawler+Text);
+            System.out.println(codigo);
+	    }else{
+	        JOptionPane.showMessageDialog(new JFrame(),"No se ha podido cargar la página.\nEl Status code que "+"devuelve la página es: "+codigo);
+	   }
+		System.exit(0);
+        
+        Set<String> visitedUrls = new HashSet<>();
+        String url = "https://example.com";
+        crawl(url, visitedUrls);
+	}
+
+    private static void crearOComprobarArchivo(String url, String crawler){}
+    private static void crawl(String url, Set<String> visited) {}
 }
--- Crawler/Acceso_Fichero.java
+++ Crawler/Acceso_Fichero.java
@@ -1,14 +1,29 @@
 package  Crawler;
 import java.io.*;
+import java.net.URI;
+import java.net.http.HttpResponse;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 import java.net.*;
+
+
 
 public class Acceso_Fichero{
 
-    public void read(){
-              
+    public void read() throws InterruptedException{
+        String URL = "https://example.com";
         try{
 
-            URL url = new URL("https://example.com");
+            HttpClient Client = HttpClient.newHttpClient();
+            HttpRequest Request = HttpRequest.newBuilder()
+                .uri(URI.create(URL))
+                .build();
+            HttpResponse<String> response = Client.send(Request, HttpResponse.BodyHandlers.ofString());
+            String code = response.body();
+            System.out.println(code);
+
+            URL url = new URL("https://example.com/");
             BufferedReader entrada = new BufferedReader(new InputStreamReader(url.openStream()));
             File file = new File ("body.html");
             BufferedWriter salida = new BufferedWriter(new FileWriter(file));
@@ -24,8 +39,8 @@ public class Acceso_Fichero{
         }catch(IOException e) {
             System.out.println("No se ha encontrado el archivo");
         }
+
     }
 }
-
 
 
